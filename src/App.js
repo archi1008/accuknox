f96382b2625f9a7fff3ec709f906e0579314de3c
archi1008 <gargarchi941@gmail.com>
2024-03-23 13:35:22 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFf9hw92TGmXQ55WAAecX5DJVYQcxF9NU",
+  authDomain: "accuknox-b6742.firebaseapp.com",
+  projectId: "accuknox-b6742",
+  storageBucket: "accuknox-b6742.appspot.com",
+  messagingSenderId: "770394312927",
+  appId: "1:770394312927:web:b67ca8a73dc7af08ad596a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
